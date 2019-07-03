95228f9a7c7420a46d8d5e96b73bd91ec72bc1a6
Pedro R. Jiménez <ptpedroj@gmail.com>
2019-07-03 00:37:09 -0400

    More Setup; Enabled Flow; Refactored For Flow

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react';
+import Styles from './App.module.css';
+
+const app = () => (
+  <div className={Styles.App}>
+    <h1>Burger Builder</h1>
+  </div>
+);
+
+export default app;
