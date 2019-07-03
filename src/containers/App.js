251158385f3d7041392697f85526d5e9081f6581
Pedro R. Jiménez <ptpedroj@gmail.com>
2019-07-03 00:49:08 -0400

    Converted App to Class Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,16 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import Styles from './App.module.css';
 
-const app = () => (
-  <div className={Styles.App}>
-    <h1>Burger Builder</h1>
-  </div>
-);
+type Props = {};
 
-export default app;
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component<Props> {
+  render = () => (
+    <div className={Styles.App}>
+      <h1>Burger Builder</h1>
+    </div>
+  );
+}
+
+export default App;
