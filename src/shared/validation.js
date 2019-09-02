57c69d062e861505c006f56149e5c37cd6ca76af
Pedro R. Jiménez <countzyx@gmail.com>
2019-09-02 04:06:07 -0400

    Refactored getErrorMessage to Its Own File

--- src/shared/validation.js
+++ src/shared/validation.js
@@ -1,6 +1,7 @@
 // @flow
 import type { FormElementValidationRules } from '../types';
 
+// eslint-disable-next-line import/prefer-default-export
 export const getErrorMessage = (value: string, rules: FormElementValidationRules) => {
   const trimmedValue = value.trim();
 
