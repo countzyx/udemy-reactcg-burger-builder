d00b0772b49d4b96cda22b84fed434e48460ed17
Pedro R. Jiménez <countzyx@gmail.com>
2019-09-02 04:47:57 -0400

    Conditionally Enable Redux Tools Only in Dev

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ const rootReducer = combineReducers({
   orders: ordersReducer,
 });
 // eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null)
+  || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const root: ?Element = document.getElementById('root');
