fec13098fd34789f368173a867bfaa22b685c90c
Pedro R. Jiménez <countzyx@gmail.com>
2019-09-14 02:09:57 -0400

    Refactored Auth to a Functional Component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,153 +43,163 @@ type State = {
   isSignUp: boolean,
 };
 
-class Auth extends React.Component<Props, State> {
-  state = {
-    authForm: {
-      email: {
-        elementType: 'input',
-        elementConfig: {
-          options: null,
-          placeholder: 'Your email address',
-          type: 'email',
-        },
-        label: 'Email',
-        touched: false,
-        valid: true,
-        validation: {
-          required: true,
-          isEmail: true,
-        },
-        validationError: null,
-        value: 'test@test.com',
+const initialFormState: State = {
+  authForm: {
+    email: {
+      elementType: 'input',
+      elementConfig: {
+        options: null,
+        placeholder: 'Your email address',
+        type: 'email',
       },
-      password: {
-        elementType: 'input',
-        elementConfig: {
-          options: null,
-          placeholder: '',
-          type: 'password',
-        },
-        label: 'Password',
-        touched: false,
-        valid: true,
-        validation: {
-          required: true,
-          minLength: 6,
-        },
-        validationError: null,
-        value: 'test@test.com',
+      label: 'Email',
+      touched: false,
+      valid: true,
+      validation: {
+        required: true,
+        isEmail: true,
       },
+      validationError: null,
+      value: 'test@test.com',
     },
-    formIsValid: true,
-    isSignUp: false,
-  };
+    password: {
+      elementType: 'input',
+      elementConfig: {
+        options: null,
+        placeholder: '',
+        type: 'password',
+      },
+      label: 'Password',
+      touched: false,
+      valid: true,
+      validation: {
+        required: true,
+        minLength: 6,
+      },
+      validationError: null,
+      value: 'test@test.com',
+    },
+  },
+  formIsValid: true,
+  isSignUp: false,
+};
 
-  componentDidMount = () => {
-    const { building, redirectPath, onSetAuthRedirectPath } = this.props;
+const Auth = (props: Props) => {
+  const [formState, setFormState] = React.useState(initialFormState);
+  const {
+    building,
+    error,
+    loading,
+    redirectPath,
+    onLogin,
+    onSetAuthRedirectPath,
+    onSignUp,
+    userAuthenticated,
+  } = props;
+
+  React.useEffect(() => {
     if (!building && redirectPath !== '/') {
       onSetAuthRedirectPath('/');
     }
-  };
+  }, [building, redirectPath, onSetAuthRedirectPath]);
 
-  inputChangedHandler = (event: SyntheticEvent<HTMLInputElement>, id: string) => {
-    const { authForm } = this.state;
-    const updatedAuthForm = {
-      ...authForm,
-    };
-    const updatedFormElement = {
-      ...updatedAuthForm[id],
-    };
+  const changeInputHandler = React.useCallback(
+    (event: SyntheticEvent<HTMLInputElement>, id: string) => {
+      const { authForm } = formState;
+      const updatedAuthForm = {
+        ...authForm,
+      };
+      const updatedFormElement = {
+        ...updatedAuthForm[id],
+      };
 
-    updatedFormElement.value = event.currentTarget.value;
-    updatedFormElement.validationError = getErrorMessage(
-      updatedFormElement.value,
-      updatedFormElement.validation,
-    );
-    updatedFormElement.valid = !updatedFormElement.validationError;
-    updatedFormElement.touched = true;
-
-    updatedAuthForm[id] = updatedFormElement;
-
-    const formIsValid = Object.keys(updatedAuthForm).reduce<boolean>(
-      // eslint-disable-next-line max-len
-      (acc, k) => (updatedAuthForm[k].valid === undefined ? acc : acc && updatedAuthForm[k].valid),
-      true,
-    );
-
-    this.setState({ formIsValid, authForm: updatedAuthForm });
-  };
-
-  loginHandler = (event: SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const { authForm, isSignUp } = this.state;
-    if (!authForm) {
-      return;
-    }
-
-    const { onLogin, onSignUp } = this.props;
-    const action = isSignUp ? onSignUp : onLogin;
-    action(authForm.email.value, authForm.password.value);
-  };
-
-  switchAuthModeHandler = () => {
-    this.setState(prevState => ({ isSignUp: !prevState.isSignUp }));
-  };
-
-  render = () => {
-    const {
-      error, loading, redirectPath, userAuthenticated,
-    } = this.props;
-    if (userAuthenticated) {
-      return <Redirect to={redirectPath} />;
-    }
-
-    if (loading) {
-      return <Spinner />;
-    }
-
-    const { authForm, formIsValid, isSignUp } = this.state;
-    if (!authForm) {
-      return <p>No order form present.</p>;
-    }
-
-    const formElements = Object.keys(authForm).map((key) => {
-      const config: FormElement = authForm[key];
-      return (
-        <Input
-          changed={event => this.inputChangedHandler(event, key)}
-          key={key}
-          elementConfig={config.elementConfig}
-          id={key}
-          inputType={config.elementType}
-          invalid={!config.valid}
-          label={config.label}
-          shouldValidate={config.validation !== undefined}
-          touched={config.touched}
-          validationError={config.validationError}
-          value={config.value}
-        />
+      updatedFormElement.value = event.currentTarget.value;
+      updatedFormElement.validationError = getErrorMessage(
+        updatedFormElement.value,
+        updatedFormElement.validation,
       );
-    });
+      updatedFormElement.valid = !updatedFormElement.validationError;
+      updatedFormElement.touched = true;
 
+      updatedAuthForm[id] = updatedFormElement;
+
+      const formIsValid = Object.keys(updatedAuthForm).reduce<boolean>(
+        // eslint-disable-next-line max-len
+        (acc, k) => (updatedAuthForm[k].valid === undefined ? acc : acc && updatedAuthForm[k].valid),
+        true,
+      );
+
+      setFormState(prevState => ({ ...prevState, formIsValid, authForm: updatedAuthForm }));
+    },
+    [formState, setFormState],
+  );
+
+  const loginHandler = React.useCallback(
+    (event: SyntheticEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const { authForm, isSignUp } = formState;
+      if (!authForm) {
+        return;
+      }
+      const onAction = isSignUp ? onSignUp : onLogin;
+      onAction(authForm.email.value, authForm.password.value);
+    },
+    [formState, onLogin, onSignUp],
+  );
+
+  const switchAuthModeHandler = React.useCallback(() => {
+    setFormState(prevState => ({ ...prevState, isSignUp: !prevState.isSignUp }));
+  }, [setFormState]);
+
+  if (userAuthenticated) {
+    return <Redirect to={redirectPath} />;
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  const { authForm, formIsValid, isSignUp } = formState;
+  if (!authForm) {
+    return <p>No order form present.</p>;
+  }
+
+  const formElements = Object.keys(authForm).map((key) => {
+    const config: FormElement = authForm[key];
     return (
-      <div className={styles.Auth}>
-        {error ? <p className={styles.Error}>{error.message}</p> : null}
-        <h4>Enter your contact data</h4>
-        <form onSubmit={this.loginHandler}>
-          {formElements}
-          <Button buttonType="Success" isDisabled={!formIsValid}>
-            {isSignUp ? 'Sign Up' : 'Login'}
-          </Button>
-        </form>
-        <Button buttonType="Danger" clicked={this.switchAuthModeHandler}>
-          {`Switch to ${isSignUp ? 'Login' : 'Signup'}`}
-        </Button>
-      </div>
+      <Input
+        changed={event => changeInputHandler(event, key)}
+        key={key}
+        elementConfig={config.elementConfig}
+        id={key}
+        inputType={config.elementType}
+        invalid={!config.valid}
+        label={config.label}
+        shouldValidate={config.validation !== undefined}
+        touched={config.touched}
+        validationError={config.validationError}
+        value={config.value}
+      />
     );
-  };
-}
+  });
+
+  return (
+    <div className={styles.Auth}>
+      {error ? <p className={styles.Error}>{error.message}</p> : null}
+      <h4>Enter your contact data</h4>
+      <form onSubmit={loginHandler}>
+        {formElements}
+        <Button buttonType="Success" isDisabled={!formIsValid}>
+          {isSignUp ? 'Sign Up' : 'Login'}
+        </Button>
+      </form>
+      <Button buttonType="Danger" clicked={switchAuthModeHandler}>
+        {`Switch to ${isSignUp ? 'Login' : 'Signup'}`}
+      </Button>
+    </div>
+  );
+};
 
 export default connect(
   mapStateToProps,
